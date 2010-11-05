78da37a393ab95015a2c66be6711ad65db6e325a
Alessandro Pasotti <apasotti@gmail.com>
2010-11-05 19:23:54 +0100

    Grass api example, now working

--- examples/grass_api.py
+++ examples/grass_api.py
@@ -5,35 +5,32 @@ import sys, os
 GISBASE = '/usr/lib/grass64/'
 wd = os.path.dirname(os.path.realpath(__file__))
 
+# Setup environment
 sys.path.append( GISBASE + 'etc/python/' )
 os.environ['GISBASE'] = GISBASE
 os.environ['GISRC'] = '/home/' + os.environ['USER'] + '/.grassrc6'
 os.environ['PATH'] = os.environ['PATH'] + ':' + GISBASE + 'scripts/'
 os.environ['PATH'] = os.environ['PATH'] + ':' + GISBASE + 'bin/'
 os.environ['LD_LIBRARY_PATH'] = GISBASE + 'lib/'
+os.environ['GIS_LOCK'] = "%s" % os.getpid()
 
 import grass.script as grass
 
 print grass.gisenv()
+
+import shutil
+try:
+    shutil.rmtree(grass.gisenv()['GISDBASE'] + '/regioni')
+except:
+    pass     
+
 print grass.run_command('g.version', flags = 'r')
-
-print wd + '/../data/regioni.shp'
 print grass.run_command('v.in.ogr', flags = 'l', dsn = wd + '/../data/regioni.shp')
-
-# Ctypes
-
-
-import subprocess
-from ctypes import *
-grass = CDLL("libgrass_gis.so")
-
-input = raw_input("Raster Map Name? ")
-
-# initialize
-s = subprocess.Popen(['g.version','-r'], stdout=subprocess.PIPE).communicate()[0]
-for line in s.splitlines():
-    if line.startswith('Revision:'):
-        version = '$' + line + '$'
-grass.G__gisinit(version, '')
+print grass.run_command('v.in.ogr', flags='c', layer = 'regioni', location = 'regioni', output = 'regioni',  dsn = wd + '/../data/regioni.shp')
+# Set region resolution
+print grass.run_command('g.mapset', mapset='PERMANENT', location='regioni')
+print grass.run_command('g.region', res = 0.02)
+print grass.run_command('g.list',  type = 'vect')
+print grass.run_command('v.to.rast', input='regioni', output='regioni', column='cod_reg')
 
 
