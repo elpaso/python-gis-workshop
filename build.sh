c03b4a6f8e3f4053fd3956f3b59433fa386642cc
Alessandro Pasotti <apasotti@gmail.com>
2010-11-10 14:46:27 +0100

    New 'python' theme

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 NAME=$1
-THEME=django
-#THEME=default
+THEME=$2
+if [ -z "$THEME" ] ; then
+  THEME=django
+fi
 python rst-directive.py \
     --stylesheet=pygments.css \
     --theme-url=ui/${THEME} \
